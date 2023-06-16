bdc1c624d10097768706971f0757af4ca4de0bdb
dez <godlyrics7@gmail.com>
2023-06-16 04:56:11 +0200

    Init i18n

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 import App from './App.vue'
+import i18n from './plugins/i18n'
 import vuetify from './plugins/vuetify'
 import VueForceNextTick from 'vue-force-next-tick'
 
@@ -14,6 +15,7 @@ const vue = createApp(App);
 vue.use(VueAxios, axios)
 vue.use(router);
 vue.use(store);
+vue.use(i18n);
 vue.use(vuetify);
 vue.use(VueForceNextTick)
 
---
+++ src/plugins/i18n.js
@@ -0,0 +1,20 @@
+// Imports
+import { createI18n } from 'vue-i18n'
+
+const messages = Object.fromEntries(
+    Object.entries(
+        import.meta.glob('../i18n/messages/*.json', { eager: true }))
+      .map(([key, value]) => {
+        return [key.slice(key.lastIndexOf('/') + 1, -5), (value).default]
+    }),
+)
+
+export default createI18n({
+    legacy: false,
+    locale:  'en',
+    messages,
+})
+
+/*watch(() => localeStore.locale, locale => {
+    i18n.global.locale.value = locale
+})*/
