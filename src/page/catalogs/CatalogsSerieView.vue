678c2102197f3380fbdb2500357eab615c0f72c2
dez <godlyrics7@gmail.com>
2023-06-21 02:57:46 +0200

    Added catalogs default render

--- src/page/catalogs/CatalogsSerieView.vue
+++ src/page/catalogs/CatalogsSerieView.vue
@@ -2,26 +2,98 @@
   <CatalogsTopBar :showAppBar="true">
     <template #main>
       <div>
-        <iframe id="inlineFrameExample"
-          title="Inline Frame Example"
-          style="width: 0; min-width: 100% !important;" height="500"
-          src="http://localhost:8080">
-        </iframe>
+        
+        <VideoTabs :tabs="tabs">
+          <template #Series>
+            <v-row>
+              <v-col cols="8">
+                <SeriesColGrid
+                  state="match"
+                  :series="series"
+                  :getSerie="getSerie"
+                  :previewSeasons="setSerie"
+                />
+              </v-col>
+              <v-col cols="4">
+                <SeasonForm 
+                  :serie="serie"
+                />
+              </v-col>
+            </v-row>
+          </template>
+          <template #Preview>
+            <iframe id="inlineFrameExample"
+              title="Inline Frame Example"
+              style="width: 0; min-width: 100% !important;" height="800"
+              src="http://localhost:8080">
+            </iframe>
+          </template>
+        </VideoTabs>
       </div>
     </template>
   </CatalogsTopBar>
 </template>
 <script>
+import apiMixin from "@/mixins/apiMixin"
+import VideoTabs from "@/components/tabs/VideoTabs.vue"
 import CatalogsTopBar from "@/components/nav/CatalogsTopBar.vue"
+import SeriesColGrid from "@/components/grids/SeriesColGrid.vue"
+import SeasonForm from "@/components/form/SeasonForm.vue"
 
 export default {
   name: 'CatalogsSerieView',
   components : {
+    VideoTabs,
+    SeasonForm,
+    SeriesColGrid,
     CatalogsTopBar
   },
-  data: () => ({ 
-    items: [], 
-  })
+  data: () => ({
+    serie : null, 
+    items: [],
+    series : [],
+    tabs : [
+      {
+        id : "1",
+        title : "Series"
+      },
+      {
+        id : "2",
+        title : "Preview"
+      }
+    ], 
+  }),
+  mixins : [
+    apiMixin
+  ],
+  methods : {
+    getSerie : function(params, callBack) {
+      this.getData(import.meta.env.VITE_MABOKE_API_ROOT + "/serie", params, (series) => {
+        let pageable 
+        const {number, totalPages} = series
+        if(series) {
+          pageable = {
+            pageNumber : number,
+            totalPages :  totalPages
+          }
+
+          if(params.state === "match") {
+            this.series = series.content            
+          } 
+
+          if(callBack) {
+            callBack(pageable)
+          }
+          //console.log("Serie : ", series)
+        }
+      })
+    },
+    setSerie : function(serie) {
+      if(serie) {
+        this.serie = serie
+      }
+    }
+  }
 }
 </script>
 <style>
